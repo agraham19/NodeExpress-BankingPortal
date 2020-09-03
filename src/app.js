3253fd500eaf11dedcfb4237e79cb93ad2029706
Andrew Graham <andrew.graham@digital.homeoffice.gov.uk>
2020-09-03 17:07:36 +0100

    fix

--- src/app.js
+++ src/app.js
@@ -27,13 +27,26 @@ app.get('/checking', (req, res) => res.render('account', { account: accounts.che
 app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
 app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
 app.get('/transfer', (req, res) => res.render('transfer'));
+
+app.get('/transfer', (req, res) =>  res.render('transfer'));
+
 app.post('/transfer', (req, res) => {
-  accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
-  accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) +
-      parseInt(req.body.amount, 10);
-  const accountsJSON = JSON.stringify(accounts, null, 4);
-  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+  accounts[req.body.from].balance -= req.body.amount;
+  accounts[req.body.to].balance += parseInt(req.body.amount, 10);
+  let accountsJSON = JSON.stringify(accounts, null, 4)
+  fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
   res.render('transfer', {message: 'Transfer Completed'});
 });
+
+app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit}));
+app.post('/payment', (req, res) => {
+  accounts.credit.balance -= req.body.amount;
+  accounts.credit.available += parseInt(req.body.amount);
+  let accountsJSON = JSON.stringify(accounts, null, 4)
+  fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+  res.render('payment', {message: 'Payment Successful', account: accounts.credit});
+});
+
+
 
 app.listen(3000, () => console.log('PS Project Running on port 3000'));
